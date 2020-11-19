2b17dd322b3160a5f3f7009af7035d0d71490936
luismbmelo <luismbmelo@gmail.com>
2020-11-19 12:23:51 +0100

    fixing processos.csv path

--- src/Projects.js
+++ src/Projects.js
@@ -10,7 +10,7 @@ class Projects extends React.Component {
     };
     this.splitter = this.splitter.bind(this);
 
-      readRemoteFile("http://localhost:5000/projectos.csv", {
+      readRemoteFile("https://miguelzurkcruz.com/projectos.csv", {
             complete: (results) => {
               this.splitter(results['data']);
             },
